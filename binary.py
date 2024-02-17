6becb26a7358307fa9a04578808f868a0850f278
JianZcar <jianstbn@gmail.com>
2024-02-17 11:12:43 +0800

    making sure that the binary is 16 bits

--- binary.py
+++ binary.py
@@ -30,7 +30,57 @@ def binary_to_decimal(binary: str = ''):
     else:
         decimal = inner(binary, 0, True, True)
 
-    print(decimal)
+    print(binary)
+    return binary
 
 
-binary_to_decimal()
+def twos_compliment(binary: str = ''):
+    binary = input('Enter Binary -> ') if binary == '' else binary
+
+    if not is_binary(binary):
+        print('Input Error')
+        return None
+
+    # Reverse the bits
+    binary = ''.join('1' if bit == '0' else '0' for bit in binary)
+
+    # Add 1 to the result
+    binary = bin(int(binary, 2) + 1)[2:]
+
+    print(binary)
+    return binary
+
+
+def four_bit_spacer(binary: str = ''):
+    binary = input('Enter Binary -> ') if binary == '' else binary
+
+    if not is_binary(binary):
+        print('Input Error')
+        return None
+
+    # Add a space every four bits
+    binary = ' '.join([binary[i:i+4] for i in range(0, len(binary), 4)])
+
+    return binary
+
+
+def make_16_bits(binary: str = ''):
+    binary = input('Enter Binary -> ') if binary == '' else binary
+
+    if not is_binary(binary):
+        print('Input Error')
+        return None
+
+    if len(binary) > 16:
+        print('Input binary is more than 16 bits')
+        return None
+
+    # Pad with leading zeros
+    binary = binary.zfill(16)
+    binary = four_bit_spacer(binary)
+
+    print(binary)
+    return binary
+
+
+make_16_bits()
